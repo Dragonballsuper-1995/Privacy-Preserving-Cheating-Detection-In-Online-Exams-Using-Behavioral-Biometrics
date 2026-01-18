3da61292bc0900c0ef7e360d40904a6059bb6159
SujalChhajed925 <sujalchhajed925@gmail.com>
2026-01-18 22:42:57 +0530

    fix(ci): add frontend Dockerfile and optimize backend build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {},
+  allowedDevOrigins: ["localhost", "192.168.89.1"],
+  output: "standalone",
 };
 
 export default nextConfig;
